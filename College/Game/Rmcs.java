04ef7d76dd1958ba31e93e920856a35b8903c118
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2022-09-05 20:44:00 +0530

    Game

---
+++ College/Game/Rmcs.java
@@ -0,0 +1,25 @@
+//Chor Sipahi Game Project
+import java.util.*;
+public class Rmcs {
+    public static void main(String[] args) {
+        // Creae Option choose your option
+        System.out.println("Choose your option");
+        System.out.println("1.Raja");
+        System.out.println("2.Mantri");
+        System.out.println("3.Chor");
+        System.out.println("4.Sipahi");
+        // Create Scanner
+        Scanner input = new Scanner(System.in); // Create Scanner
+        int option = input.nextInt(); // Read the option
+
+        Random random = new Random(); // Create Random
+        int randomOption = random.nextInt(4) + 1; // 1 to 4 random number 
+        // Create if else
+        if (option == randomOption) {  // If option is equal to randomOption
+            System.out.println("You Win"); // Print You Win
+        } else {
+            System.out.println("You Lose");  // Print You Lose
+        }
+
+    }
+}
