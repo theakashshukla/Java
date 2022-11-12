cb7174f9a483e0e84f453f0e93a517ded1a55bf0
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2022-11-12 15:50:23 +0530

    Concat String

---
+++ Project 15/ConcatString.java
@@ -0,0 +1,10 @@
+// Write a Program in Java to Concatenate Two Strings Without Using Library Function.
+
+public class ConcatString {
+    public static void main(String[] args){
+        String s1 = "Hello";
+        String s2 = "World";
+        String s3 = s1 + s2;
+        System.out.println(s3);
+    }
+}
