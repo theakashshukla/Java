25ea8b073a308419c2ada1cd1fe4fc6773d545bb
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2022-11-30 21:28:02 +0530

    Project 12 & 13

--- Project 12/AccessClassMember.java
+++ Project 12/AccessClassMember.java
@@ -1,9 +1,14 @@
 // Write a Program in Java to Show Accessing Class Members and use a Dot(.).
 public class AccessClassMember {
     public static void main(String[] args){
-        System.out.println("Hello");
-
+    
+        AccessClassMember obj = new AccessClassMember();
+    
+        obj.myMethod();
     }
 
-    
+    public void myMethod(){
+        System.out.println("Accessing Class Members");
+    }
 }
+
