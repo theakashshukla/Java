ebb7bf2a7ffdb14e25139fcf55099b9cd2d2a533
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2022-12-01 11:09:30 +0530

    Project 14 Added

---
+++ Project 14/InheritS.java
@@ -0,0 +1,14 @@
+// Write a Program in Java to Show Single Inheritance.
+
+class Display{
+    void show(){
+        System.out.println("Showing...");
+    }
+}
+
+public class InheritS {
+    public static void main(String[] args){
+        Display s = new Display();
+        s.show();
+    }
+}
