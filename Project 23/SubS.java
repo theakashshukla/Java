b7a926d5dff86c7632ca68586b82efb8a8b09cca
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2022-12-15 22:49:44 +0530

    Done 23 & 24

---
+++ Project 23/SubS.java
@@ -0,0 +1,9 @@
+// Write a Program in Java to Get Sub String from a Given String.
+
+public class SubS {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println(str.substring(0, 5));
+        System.out.println(str.substring(6));
+    }
+}
---
+++ Project 24/StringLtoU.java
@@ -0,0 +1,10 @@
+// Java Program to Convert String to Lowercase and Uppercase.
+
+public class StringLtoU {
+    public static void main(String[] args){
+        String str = "Hello World";
+        System.out.println("String: " + str);
+        System.out.println("Lowercase: " + str.toLowerCase());
+        System.out.println("Uppercase: " + str.toUpperCase());
+    }
+}
